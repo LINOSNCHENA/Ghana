9c26aa0d01beca60aac1c23381f17fd101165b62
Marvin Nchena <linosnchena@gmail.com>
2025-06-17 23:05:39 +0200

    germany

--- src/app/utils/constants.ts
+++ src/app/utils/constants.ts
@@ -146,5 +146,3 @@ export const fotoNames = [
     '20. Ghana Independence Day Celebration Marking National Pride, Progress, and Unity Date: March 6th | Location: Nationwide, Ghana',
 
 ]
-
-export const specialists = ['20295', 'shfgf'];
